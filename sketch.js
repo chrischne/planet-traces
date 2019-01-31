1498a9480e0b799d8d50125b03f4b81c36b69b0d
chrischne <chrischne@gmail.com>
2019-01-31 21:45:00 +0100

    basic setup with planet traces

--- sketch.js
+++ sketch.js
@@ -4,7 +4,21 @@ var ready = false;
 var goldenRatio = 1.61803398875;
 var data = [];
 var planets = ['sun', 'mercury', 'venus', 'moon', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune', 'pluto'];
-let file = 'statistics21022018-1707_planets_set.csv';
+let focusPlanet = 'mars';
+let baseUrl = 'http://127.0.0.1:5000/planet/';
+let GEO = 'geo';
+let HELIO = 'helio';
+
+let startDate = new Date(2000,0,1);
+let endDate = new Date(2001,0,1);
+
+let radScale = 50000;
+
+//let prevPos = null;
+//let pos = null;
+let positions = [];
+
+let slider;
 
 let sfMedium;
 let sfBold;
@@ -17,14 +31,63 @@ function preload() {
 
 function setup() {
   createCanvas(800, 600);
+noLoop();
+  slider = createSlider(startDate.getTime(),endDate.getTime(),startDate.getTime(),1);
+  slider.position(100,100);
+  slider.style('width','400px');
+  slider.input(update);
+ 
 
-  noLoop();
+
+  // let url = baseUrl + GEO + '/mercury/' + [1,1,1999].join('/');
+  // console.log(url);
+  // console.log(startDate);
+  // console.log(endDate);
+  //loadJSON()
   
 }
 
 function draw() {
   background('rgb(250,250,250)');
   
+  let val = slider.value();
+  let date = new Date(val);
+  noStroke();
+  fill(0);
+  text(date.toDateString(),100,100);
 
+  //console.log(date);
+  push();
+  translate(width/2,height/2);
+  noFill();
+  stroke(0);
+  beginShape();
+  for (let i = 0; i < positions.length; i++) {
+    const pos = positions[i];
+    vertex(pos.x,pos.y);
+  }
+  endShape();
+  pop();
 
+}
+
+function update(){
+  console.log('hi');
+  let val = slider.value();
+  let date = new Date(val);
+  let day = date.getDate();
+  let month = date.getMonth() +1;
+  let year = date.getFullYear();
+  let url = baseUrl + GEO + '/' + focusPlanet +'/' + [day,month,year].join('/');
+  console.log(url);
+  loadJSON(url,updatePos);
+}
+
+function updatePos(planet){
+  console.log('updatePos');
+  angle = planet.pos;
+  let r = radScale*planet.distance;
+  let v = p5.Vector.fromAngle(radians(angle),r);
+  positions.push(v);
+  redraw();
 }
